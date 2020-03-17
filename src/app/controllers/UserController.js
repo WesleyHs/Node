ee049f10b84d5548a1c4d3f28e9049526e313c62
WesleyHs <wesley.henrique.pgm@gmail.com>
2020-03-17 11:36:01 -0300

    validação do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup'; //vai importar tudo que tem dentro do yup
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    //validação
+    const schema = Yup.object().shape({
+      name: Yup.string().required(), //obrigatorio
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),//minimo 6 digitos
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } }); // verificando se o usuario ja existe pelo email
 
     if (userExists) {
@@ -19,6 +32,27 @@ class UserController {
   }
 
   async update(req, res) { //atualizar
+
+    //validação
+    const schema = Yup.object().shape({
+      name: Yup.string(), 
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),//minimo 6 digitos
+      //field se refere ao password
+      password: Yup.string().min(6).when(
+        'oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+      //confirmação de senha
+      confirmPassword: Yup.string().when('password', (password, field) => //ref referente
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
@@ -37,7 +71,7 @@ class UserController {
     }
 
     const { id, name, provider } = await user.update(req.body);
-    
+
     return res.json({
       // retornar apenas esses dados
       id,
@@ -46,7 +80,7 @@ class UserController {
       provider,
     });
 
-    return res.json({ok:true})
+    return res.json({ ok: true })
   }
 }
 
