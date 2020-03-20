26f0bc9cf1f6de9f1bd96167c4b1b49c7abd3dcd
WesleyHs <wesley.henrique.pgm@gmail.com>
2020-03-20 18:01:34 -0300

    update

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -13,7 +13,7 @@ class SessionController {
       password: Yup.string().required(),
     });
 
-    if (!(await schema.isValid(req.body))) {
+    if (!(await schema.isValid(req.body))) { 
       return res.status(400).json({ error: 'Validation fails' })
     }
 
