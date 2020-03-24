176071076081b66c716c3dbb225daa2a468f0b59
Wesley <wesley.henrique.pgm@gmail.com>
2020-03-23 22:05:59 -0300

    teste

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,7 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+
 import databaseConfig from '../config/database';
 
 
@@ -7,12 +10,12 @@ import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
 
 
-
 const models = [User, File, Appointment];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
@@ -20,6 +23,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27018/gobarber',
+      { useNewUrlParser: true, useUnifiedTopology: true}
+    )
   }
 }
 
